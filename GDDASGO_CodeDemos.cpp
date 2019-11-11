8f83b651e258ebab7097612ef7ae3788effa6450
NeilDG <fm.atropos@gmail.com>
2019-11-11 20:48:00 +0800

    Added p3 tester

--- GDDASGO_CodeDemos.cpp
+++ GDDASGO_CodeDemos.cpp
@@ -1,232 +1,221 @@
-// GDDASGO_CodeDemos.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// P3_Villanueva.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Author: Christian Garret U. Villanueva X-22 11839775 GD-DASGO
 
 #include <iostream>
-#include <string>
-#include <vector>
 #include <sstream>
-//#include <bits/stdc++.h>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <string>
 
 using namespace std;
-void combiRecur(int index, int k, vector<int> num, vector<int> combi) {
-	if (k == 0) {
-		for (int i = 0; i < combi.size(); i++) {
-			cout << combi[i] << " ";
-		}
-		cout << "\n";
-	}
-	else {
-		for (int i = index; i <= num.size() - k; i++) {
-			combi.push_back(num[i]);
-			combiRecur(i + 1, k - 1, num, combi);
-			combi.pop_back();
-		}
-	}
-}
 
-void initCombi(int n, int k) {
-	vector<int> num;
-	vector<int> combi;
+// PROBLEM 1
+void problem_p1(vector<string> input) {
+	int i, j, nFlag, nTestcases = 0, nStringLenght = 0;
+	string strHolder;
+	char cHolder;
+	stack<char> stkParen;
 
-	for (int i = 0; i < n; i++) {
-		num.push_back(i + 1);
-	}
+	stringstream ssTestcases(input[0]);
+	ssTestcases >> nTestcases;
 
-	combiRecur(0, k, num, combi);
-}
+	// Conditions
+	if (nTestcases <= input.size() - 1) {
+		for (i = 1; i <= nTestcases; i++) {
 
-void problem_p1(string input) {
-	stringstream ss;
-	int n = 0, k, check; // n = max num // k = num of elements	 
-	string temp;
+			nFlag = 1;
 
-	ss << input;
-
-	while (!ss.eof()) {
-		ss >> temp;
-		if (stringstream(temp) >> check && n == 0) {
-			stringstream(temp) >> n;
-		}
-		else if (stringstream(temp) >> check)
-			stringstream(temp) >> k;
-	}
-
-	initCombi(n, k);
-}
-
-vector<int> getLine(string input) {
-	vector<int> temp;
-	string num;
-	stringstream check(input);
-
-	while (getline(check, num, ' ')) {
-		temp.push_back(atoi(num.c_str()));
-	}
-
-	return temp;
-}
-
-void initBox(vector <int> num, vector<int> converted) {
-	int box[3];
-	bool fit;
-
-	for (int i = 0; i < num[0]; i++) {
-		fit = true;
-		for (int j = 0; j < 3; j++) {
-			box[j] = converted[((3 * i) + j)];
-			if (box[j] > 20) {
-				fit = false;
+			while (!stkParen.empty()) {
+				stkParen.pop();
 			}
-			cout << box[j] << " ";
-		}
-		if (fit) {
-			cout << "CAN FIT\n";
-		}
-		else {
-			cout << "CANNOT FIT\n";
+
+			nStringLenght = input[i].size();
+
+			if (nStringLenght <= 128) {
+				strHolder = input[i];
+
+				for (j = 0; j < nStringLenght; j++)
+				{
+					if (strHolder[j] == '(' || strHolder[j] == '[')
+						stkParen.push(strHolder[j]);
+
+					if (stkParen.empty()) {
+						cout << "No" << endl;
+						nFlag = 0;
+					}
+
+					else
+					{
+						if (strHolder[j] == ')') {
+							if (stkParen.top() == '(')
+								stkParen.pop();
+							else
+								cout << "No";
+						}
+
+						if (strHolder[j] == ']') {
+							if (stkParen.top() == '[')
+								stkParen.pop();
+							else
+								cout << "No";
+						}
+					}
+				}
+				if (nFlag == 1 && stkParen.empty())
+					cout << "Yes" << endl;
+				else if (nFlag == 1)
+					cout << "No" << endl;
+			}
+			else
+				cout << "EXCEEDED MAXIUMUM STRING INPUT! \n\n";
 		}
 	}
+	else
+		cout << "THE NUMBER OF TESTCASES ARE NOT WITHIN THE RANGE OF THIS CODE! \n\n";
 }
 
+// PROBLEM 2
 void problem_p2(vector<string> input) {
-	vector<int> converted;
-	vector<int> num;
-	vector<int> temp;
+	int i, nN = 0, nT = 0, nM = 0;
+	queue<int> queFerry;
 
-	num = getLine(input[0]);
+	stringstream ssInput(input[0]);
+	ssInput >> nN >> nT >> nM;
 
-	input.erase(input.begin());
+	vector<int> nCarWaitTimeStorage(nM);
+	vector<string> sCommandStorage(nM);
 
-	for (int i = 0; i < num[0]; i++) {
-		temp = getLine(input[i]);
-		for (int j = 0; j < 3; j++) {
-			converted.push_back(temp[j]);
+	if (input.size() - 1 <= nM && nM <= 10000) {
+		for (i = 1; i < input.size(); i++)
+		{
+			//qFerry
+			stringstream ssCommand(input[i]);
+			ssCommand >> nCarWaitTimeStorage[i - 1] >> sCommandStorage[i - 1];
+		}
+
+
+
+
+	}
+	else
+		cout << "NOT WITHIN THE LIMIT!" << endl;
+}
+
+// PROBLEM 3
+void problem_p3(string input) {
+	int nLenght = 1, nInput = 0, nTemp = 0, nFlag = 0;
+	stack<int> stkInput;
+	stringstream ssInput(input);
+
+	if (input.length() > 0) {
+		for (int i = 0; input[i] != '\0'; i++) // Gets lenght without spaces 
+		{
+			if (input[i] == ' ')
+				nLenght++;
+		}
+
+		vector<int> nArray(nLenght);
+		vector<int> nArrayInc(nLenght);
+		vector<int> nArrayDec(nLenght);
+
+		if (nLenght <= 1000)
+		{
+			for (int i = 0; i < nLenght; i++) // converts string into int then, it will be put to nArray & stkInput
+			{
+				ssInput >> nInput;
+				nArray[i] = nInput;
+				stkInput.push(nInput);
+			}
+
+			nTemp = nArray[0];
+			for (int i = 0; i < nLenght; i++) // Decrementing by 1
+			{
+				nArrayDec[i] = nTemp;
+				nTemp--;
+			}
+
+			nTemp = nArray[0];
+			for (int i = 0; i < nLenght; i++) // Incrementing by 1
+			{
+				nArrayInc[i] = nTemp;
+				nTemp++;
+			}
+
+			for (int i = 0; i < nLenght; i++)
+			{
+				if (nArray[i] == nArrayDec[i] || nArray[i] == nArrayInc[i]) // Checks if Input is the same as nArrayDec or nArrayInc
+				{
+					nFlag = 0;
+					stkInput.pop();
+				}
+
+				else
+				{
+					nFlag = 1;
+					break;
+				}
+			}
+
+			if (nFlag == 0 && stkInput.empty())
+				cout << "YES" << endl;
+			else
+				cout << "NO" << endl;
 		}
 	}
-
-	initBox(num, converted);
+	else
+		cout << "THERE IS NO INPUT!";
 }
 
-void checkHarvest(vector <int> converted1, vector<int> converted2) {
-	int sum = 0, req = 0;
 
-	cout << "Number of villages: " << converted1[0] << "\n";
-	cout << "cost: " << converted1[1] << "\n";
 
-	req = converted1[0] * converted1[1];
-
-	for (int i = 0; i < converted2.size(); i++) {
-		sum += converted2[i];
-		cout << converted2[i] << " ";
-	}
-	cout << "\n";
-
-	if (req > sum) {
-		cout << "NOT ENOUGH FOOD";
-	}
-	else if (req == sum) {
-		cout << "JUST ENOUGH FOR EVERYONE";
-	}
-	else {
-		cout << "PARTY!";
-	}
-}
-
-void problem_p3(vector<string> input) {
-	vector<int> converted1;
-	vector<int> converted2;
-	int temp, n, m, check, count = 0;
-
-	converted1 = getLine(input[0]);
-	converted2 = getLine(input[1]);
-
-	checkHarvest(converted1, converted2);
-}
-
-int main()
-{
+int main() {
 	//this is an example of how your instructor will test your code by giving at least 3 test cases
 	vector<string> strInput;
-	strInput.push_back("5 3");
-	strInput.push_back("6 3");
-	strInput.push_back("4 3");
+	strInput.push_back("3");
+	strInput.push_back("([])");
+	strInput.push_back("(([()])))");
+	strInput.push_back("([()[]()])()");
 	cout << "=========================================\n";
 	cout << "#1 - Test 1 \n\n";
-	problem_p1(strInput[0]);
-	cout << "\n";
 
-	cout << "#1 - Test 2 \n\n";
-	problem_p1(strInput[1]);
-	cout << "\n";
-
-	cout << "#1 - Test 3 \n\n";
-	problem_p1(strInput[2]);
-	cout << "\n";
+	problem_p1(strInput);
 
 	cout << "=========================================\n";
 
 	strInput.clear();
-	strInput.push_back("4");
-	strInput.push_back("20 20 21");
-	strInput.push_back("1 21 1");
-	strInput.push_back("21 1 1");
-	strInput.push_back("50 50 50");
+	strInput.push_back("2 10 10");
+	strInput.push_back("0 left");
+	strInput.push_back("10 right");
+	strInput.push_back("20 left");
+	strInput.push_back("30 left");
+	strInput.push_back("40 right");
+	strInput.push_back("50 left");
+	strInput.push_back("60 left");
+	strInput.push_back("70 left");
+	strInput.push_back("80 right");
+	strInput.push_back("90 right");
+
 	cout << "#2 - Test 1 \n\n";
 	problem_p2(strInput);
 	cout << "\n";
 
-	strInput.clear();
-	strInput.push_back("4");
-	strInput.push_back("5 5 5");
-	strInput.push_back("10 10 15");
-	strInput.push_back("5 4 3");
-	strInput.push_back("1 2 20");
-	cout << "#2 - Test 2 \n\n";
-	problem_p2(strInput);
-	cout << "\n";
-
-	strInput.clear();
-	strInput.push_back("4");
-	strInput.push_back("4 4 4");
-	strInput.push_back("4 21 8");
-	strInput.push_back("3 12 19");
-	strInput.push_back("13 24 9");
-	cout << "#2 - Test 3 \n\n";
-	problem_p2(strInput);
-	cout << "\n";
-	
 	cout << "=========================================\n";
 
 	strInput.clear();
-	strInput.push_back("5 500");
-	strInput.push_back("100 0 300 100 100");
+	strInput.push_back("1 2 3 4 5 6 7 8 9 20");
+	strInput.push_back("5 4 1 2 3");
+	strInput.push_back("10 9 8 7 6 5 4 3 2 1");
 	cout << "#3 - Test 1 \n\n";
-	problem_p3(strInput);
+	problem_p3(strInput[0]);
 	cout << "\n";
 
-	strInput.clear();
-	strInput.push_back("5 500");
-	strInput.push_back("1500 0 500 0 500");
 	cout << "#3 - Test 2 \n\n";
-	problem_p3(strInput);
+	problem_p3(strInput[1]);
 	cout << "\n";
 
-	strInput.clear();
-	strInput.push_back("5 200");
-	strInput.push_back("1000 100 100 100 100");
 	cout << "#3 - Test 3 \n\n";
-	problem_p3(strInput);
+	problem_p3(strInput[2]);
 	cout << "\n";
-	
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
